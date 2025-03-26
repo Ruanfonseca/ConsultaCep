dc31e421b8f23852004e075a4409e89b4744cc9b
Ruan Fonseca <64973978+Ruanfonseca@users.noreply.github.com>
2025-03-26 15:28:11 -0300

    retirando as configurações do spring security

--- backend/src/main/groovy/com/address/manager/service/LoginService.groovy
+++ backend/src/main/groovy/com/address/manager/service/LoginService.groovy
@@ -2,17 +2,16 @@ package com.address.manager.service
 
 import com.address.manager.entity.Login
 import com.address.manager.repository.LoginRepository
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
+
+
 @Service
 class LoginService{
 
     private final LoginRepository repository
-    private final BCryptPasswordEncoder passwordEncoder
 
     LoginService(LoginRepository repository) {
         this.repository = repository
-        this.passwordEncoder = new BCryptPasswordEncoder()
     }
 
     String login(String email, String senha) {
@@ -35,7 +34,7 @@ class LoginService{
             throw new RuntimeException("Usuário já existe")
         }
 
-        Login login = new Login(email: email, senha: passwordEncoder.encode(senha))
+        Login login = new Login(email: email, senha: senha)
         return repository.save(login)
     }
     }
