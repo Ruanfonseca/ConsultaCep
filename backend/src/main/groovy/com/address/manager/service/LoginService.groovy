9172eb7ec6e67c80cc5fd84cb54739d1ab69eebe
Ruan Fonseca <64973978+Ruanfonseca@users.noreply.github.com>
2025-03-26 15:17:43 -0300

    retrocedendo no uso do jwt,por conta de problemas de configurações para o spring compativel com a versão que estou usando

--- backend/src/main/groovy/com/address/manager/service/LoginService.groovy
+++ backend/src/main/groovy/com/address/manager/service/LoginService.groovy
@@ -2,20 +2,16 @@ package com.address.manager.service
 
 import com.address.manager.entity.Login
 import com.address.manager.repository.LoginRepository
-import com.address.manager.security.JwtUtil
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
 @Service
 class LoginService{
 
     private final LoginRepository repository
-    private final JwtUtil jwtUtil
     private final BCryptPasswordEncoder passwordEncoder
 
-    LoginService(LoginRepository repository, JwtUtil jwtUtil) {
+    LoginService(LoginRepository repository) {
         this.repository = repository
-        this.jwtUtil = jwtUtil
         this.passwordEncoder = new BCryptPasswordEncoder()
     }
 
@@ -30,7 +26,7 @@ class LoginService{
             throw new RuntimeException("Senha inválida")
         }
 
-        return jwtUtil.generateToken(email)
+        return email;
     }
 
 
